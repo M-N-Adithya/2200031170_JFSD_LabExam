64bba3c012f17ece42859e070ad4cd50884e90de
2200031170_MN_Adithya <2200031170@kluniversity.in>
2024-12-07 10:27:47 +0530

    By 2200031170

---
+++ src/main/java/com/klef/jfsd/exam/controller/ClientController.java
@@ -0,0 +1,26 @@
+package com.klef.jfsd.exam.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.klef.jfsd.exam.service.ProductService;
+
+@RestController
+@RequestMapping("exam")
+public class ClientController 
+{
+		@Autowired
+		private ProductService service;
+		
+		
+		
+		@GetMapping("viewall")
+		public List<Object> getproducts()
+		{
+			return service.displayObjects();
+		}
+}
